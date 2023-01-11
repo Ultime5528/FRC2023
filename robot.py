7f0a5141c5982ddc3043a69d7ad42e23a20876ce
Adibou16 <51062288+Adibou16@users.noreply.github.com>
2023-01-11 17:07:34 -0500

    12 drivetrain (#16)
    * Copy paste everything
    
    * Drivetrain added (copied from last year)
    
    * enlever fichier mauvais
    
    Co-authored-by: Thomas McDonald <4001137@keranna.qc.ca>

--- robot.py
+++ robot.py
@@ -3,11 +3,15 @@
 import commands2
 import wpilib
 
+from subsystems.drivetrain import DriveTrain
+
 
 class Robot(commands2.TimedCommandRobot):
     def robotInit(self):
-        pass
+        self.drivetrain = DriveTrain()
 
+    def robotPeriodic(self) -> None:
+        pass
 
 if __name__ == "__main__":
     wpilib.run(Robot)
