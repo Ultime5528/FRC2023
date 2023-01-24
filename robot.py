30b58f58e2490d391e26058ca390de225e0d9ac2
Felix <filabert16@outlook.con>
2023-01-23 21:49:44 -0500

    toloading concept is working

--- robot.py
+++ robot.py
@@ -8,13 +8,13 @@ from subsystems.drivetrain import Drivetrain
 from commands.drive import Drive
 from commands.followtrajectory import FollowTrajectory
 
-
 class Robot(commands2.TimedCommandRobot):
     def robotInit(self):
         wpilib.LiveWindow.enableAllTelemetry()
         self.drivetrain = Drivetrain()
         self.stick = wpilib.Joystick(0)
         self.drivetrain.setDefaultCommand(Drive(self.drivetrain, self.stick))
+        JoystickButton(self.stick, 1).whenPressed(FollowTrajectory.toLoading(self.drivetrain))
 
 if __name__ == "__main__":
     wpilib.run(Robot)
