5d58960ae8c9132c1e4003097e70eeb38ba48e97
Quexi Zhang <3800869@keranna.qc.ca>
2023-01-28 15:58:39 -0500

    Convention by the book

--- led.py
+++ led.py
@@ -2,14 +2,10 @@ import random
 from enum import Enum
 import math
 from typing import Callable, Union, Tuple, List
-
 import commands2
 import wpilib
-
 import ports
 import numpy as np
-
-
 
 def interpoler(t, couleur1, couleur2):
     assert 0 <= t <= 1
@@ -44,17 +40,17 @@ class LEDController(commands2.SubsystemBase):
         self.led_strip.start()
         self.mode = ModeLED.NONE
 
-    def set_hsv(self, i: int, color: Color):
+    def setHSV(self, i: int, color: Color):
         self.buffer[i].setHSV(*color)
 
-    def set_all(self, color_func: Callable[[int], Color]):
+    def setAll(self, color_func: Callable[[int], Color]):
         for i in range(len(self.buffer)):
-            self.set_hsv(i, color_func(i))
+            self.setHSV(i, color_func(i))
 
     def pulse(self, color):
         t = round(254 * abs(math.cos(self.time * 2 * math.pi / 500)**3))
         hue, saturation, _ = color
-        self.set_all(lambda i: (hue, saturation, 255 - t))
+        self.setAll(lambda i: (hue, saturation, 255 - t))
 
     def rainbow(self):
         for i in range(len(self.buffer)):
@@ -64,31 +60,31 @@ class LEDController(commands2.SubsystemBase):
         self.time += 3
         self.time %= 180
 
-    def select_team(self):
+    def selectTeam(self):
         pixel_value = round(510 * math.cos((1 / (12 * math.pi)) * self.time))
         if pixel_value >= 0:
             color = (125, 255, pixel_value)
         else:
             color = (0, 255, abs(pixel_value))
-        self.set_all(lambda i: color)
+        self.setAll(lambda i: color)
 
     def ripples(self, color):
         if self.time % 10 == 0:
-            def get_color(i: int):
+            def getColor(i: int):
                 if random.random() <= (1 - (wpilib.DriverStation.getMatchTime() / 15)):
                     return color
                 else:
                     return self.black
-            self.set_all(get_color)
+            self.setAll(getColor)
 
-    def waves(self, color, nombreballons):
-        def get_color(i: int):
-            prop = 0.5 * math.cos(2 * math.pi / (20/(nombreballons+1)) * (self.time / 5 + i)) + 0.5
+    def waves(self, color):
+        def getColor(i: int):
+            prop = 0.5 * math.cos(2 * math.pi / 20 * (self.time / 5 + i)) + 0.5
             return interpoler(prop, color, self.black)
-        self.set_all(get_color)
+        self.setAll(getColor)
 
-    def half_waves(self, color):
-        def get_color(i: int):
+    def halfWaves(self, color):
+        def getColor(i: int):
             prop = 0.5 * math.cos((2 * math.pi / 50) * (self.time + i)) + 0.5
             if self.last - prop <= 0:
                 t = prop
@@ -96,25 +92,25 @@ class LEDController(commands2.SubsystemBase):
                 t = abs(prop - 1)
             self.last = prop
             return interpoler(t, color, self.black)
-        self.set_all(get_color)
+        self.setAll(getColor)
 
     def flash(self, color, speed):
         if self.time % speed == 0:
             if self.time % (speed * 2) == 0:
-                self.set_all(lambda i: color)
+                self.setAll(lambda i: color)
             else:
-                self.set_all(lambda i: self.black)
+                self.setAll(lambda i: self.black)
 
     def explode(self, color):
         if self.time % 3 == 0:
-            def get_color(i: int):
+            def getColor(i: int):
                 y = random.random()
                 if y <= self.explosiveness:
                     return interpoler(y / self.explosiveness, color, np.array([color[0], 0, 255]))
                 else:
                     return self.black
             self.explosiveness -= 0.02
-            self.set_all(get_color)
+            self.setAll(getColor)
 
 
     def periodic(self) -> None:
@@ -132,7 +128,7 @@ class LEDController(commands2.SubsystemBase):
             self.ripples(color)
         elif wpilib.DriverStation.isTeleopEnabled(): #teleop
             if ModeLED.SHOOT == self.mode:
-                self.half_waves(color)
+                self.halfWaves(color)
             elif ModeLED.INTAKE == self.mode:
                 self.waves(color, self.intake.ballCount())
             elif wpilib.DriverStation.getMatchTime() <= 1:
@@ -150,7 +146,7 @@ class LEDController(commands2.SubsystemBase):
         else:  # game hasn't started
             #print("disabled")
             if alliance == wpilib.DriverStation.Alliance.kInvalid:
-                self.select_team()
+                self.selectTeam()
             else:
-                self.pulse(color)
+                self.pulse(self.red_hsv)
         self.led_strip.setData(self.buffer)
--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import commands2
+import numpy as np
 import wpilib
 from led import LEDController
 from subsystems.drivetrain import Drivetrain
@@ -15,6 +16,7 @@ class Robot(commands2.TimedCommandRobot):
         self.drivetrain.setDefaultCommand(Drive(self.drivetrain, self.stick))
         self.led_controller = LEDController()
         JoystickButton(self.stick,1).whenPressed(self.led_controller.rainbow())
+
 
 if __name__ == "__main__":
     wpilib.run(Robot)
