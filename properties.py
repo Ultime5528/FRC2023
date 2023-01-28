5707c85c01c5e84f1ba65dbbb701a81a4b16cbb3
Étienne Beaulac <etienne.beaulac@outlook.com>
2023-01-28 10:38:11 -0500

    Fix property conventions + rename to CommonProperties

--- properties.py
+++ properties.py
@@ -1,28 +1,13 @@
-from ntcore import NetworkTableInstance
-from ntcore.util import ntproperty
-
-persistent = True
+from utils.property import autoproperty
 
 
-class _Properties:
+class _CommonProperties:
     """
-    - Respect the naming convention : "subsystem/command" _ "variable type" _ "precision"
-    - ntproperty strings are the same as their variables
+    Respect the naming convention : "subsystem/command" _ "variable type" _ "precision"
+
+    Example:
+        my_variable_name = autoproperty(0.5)  # Default value is 0.5
     """
 
 
-values = _Properties()
-
-
-def check_property_names():
-    import inspect
-    import re
-
-    lines = inspect.getsource(_Properties).splitlines()
-    pattern = re.compile(r'^\s{4}(\w+) = ntproperty\("\/Properties\/(\w+)", [^,]+, writeDefault=False, persistent=persistent\)')
-
-    for line in lines[1:]:
-        if " = ntproperty(" in line:
-            match = pattern.match(line)
-            assert match, "The following property does not respect team conventions : " + line
-            assert match.group(1) == match.group(2), f"Key and name of following property are not equal : {match.group(1)} != {match.group(2)}"
+common = _CommonProperties()
