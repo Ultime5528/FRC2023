6640f28509ea06e818d88af13b7dd8d0afb7e44d
Étienne Beaulac <etienne.beaulac@outlook.com>
2023-02-06 18:02:51 -0500

    WIP Rear wheel feedback controller

--- robot.py
+++ robot.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 import commands2
 import wpilib
+import wpimath.trajectory
 from commands2._impl.button import JoystickButton
+from wpimath._controls._controls.trajectory import TrajectoryConfig
+from wpimath.geometry import Pose2d
 
+import utils.controller
 from commands.drive import Drive
 from commands.slowdrive import SlowDrive
 from subsystems.drivetrain import Drivetrain
@@ -20,8 +24,17 @@ class Robot(commands2.TimedCommandRobot):
 
         JoystickButton(self.stick, 1).whenPressed(SlowDrive(self.drivetrain, self.stick))
 
+        config = TrajectoryConfig(10, 10)
+        self.traj = wpimath.trajectory.TrajectoryGenerator.generateTrajectory([Pose2d(0, 0, 0), Pose2d(5, 5, 1.57)], config)
+        self.drivetrain.getField().getObject("Traj2").setTrajectory(self.traj)
+        self.controller = utils.controller.RearWheelFeedbackController(self.traj)
+
         # Doit être à la fin, après que tout ait été instancié
         clear_autoproperties()
+
+    def teleopPeriodic(self):
+        self.controller.update(self.drivetrain.getPose())
+        self.drivetrain.getField().getObject("Closest").setPose(self.controller.states[self.controller.current_idx].pose)
 
 
 if __name__ == "__main__":
