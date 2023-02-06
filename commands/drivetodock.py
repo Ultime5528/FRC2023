2b329ec1da118b34bf209d1330685c6f0c2412d7
Maxence <3800945@keranna.qc.ca>
2023-02-06 17:17:32 -0500

    Responded to suggestions
    added weight to the pitch for the _calculated_speed
    added timer.reset() before timer.stop()

--- commands/drivetodock.py
+++ commands/drivetodock.py
@@ -7,9 +7,10 @@ import math
 
 
 class DriveToDock(SafeCommand):
-    minimal_drive_speed = autoproperty(0.25)
-    angle_threshold = autoproperty(0.5)
-    timer_threshold = autoproperty(2)
+    minimal_drive_speed = autoproperty(0.25)  # /1
+    angle_threshold = autoproperty(0.5)  # degrees
+    timer_threshold = autoproperty(2)  # seconds
+    pitch_weight = autoproperty(0.008)  # multiplier todo change value
 
     def __init__(self, drivetrain: Drivetrain):
         super().__init__()
@@ -23,12 +24,13 @@ class DriveToDock(SafeCommand):
 
     def execute(self) -> None:
         _pitch = self.drivetrain.getPitch()
-        _calculated_speed = math.copysign((self.minimal_drive_speed + abs(_pitch)), _pitch)
+        _calculated_speed = math.copysign((self.minimal_drive_speed + self.pitch_weight * abs(_pitch)), _pitch)
         self.drivetrain.arcadeDrive(_calculated_speed, 0)
 
         if _pitch <= self.angle_threshold and self.has_docked:
             self.timer.start()
         else:
+            self.timer.reset()
             self.timer.stop()
         if _pitch > 10:
             self.has_docked = True
