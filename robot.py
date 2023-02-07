88448fdd53fb03a4f2498346f490d41441df1bd3
Thomas McDonald <4001137@keranna.qc.ca>
2023-02-06 19:11:27 -0500

    Auto stash before merge of "main" and "origin/main"

--- robot.py
+++ robot.py
@@ -4,6 +4,8 @@ import wpilib
 from commands2.button import JoystickButton
 
 from commands.drive import Drive
+from commands.followtrajectory import FollowTrajectory
+from commands.slowdrive import SlowDrive
 from commands.gogrid import GoGrid
 from subsystems.drivetrain import Drivetrain
 from utils.property import clear_autoproperties
@@ -19,7 +21,8 @@ class Robot(commands2.TimedCommandRobot):
         self.drivetrain.setDefaultCommand(Drive(self.drivetrain, self.stick))
         JoystickButton(self.stick, 1).whenPressed(GoGrid(self.drivetrain, "3"))
 
-        # JoystickButton(self.stick, 1).whenPressed(SlowDrive(self.drivetrain, self.stick))
+        JoystickButton(self.stick, 1).whenPressed(SlowDrive(self.drivetrain, self.stick))
+        JoystickButton(self.stick, 2).whenPressed(FollowTrajectory.driveStraight(self.drivetrain, 25, 0.1))
 
         # Doit être à la fin, après que tout ait été instancié
         clear_autoproperties()
