b2683c931ce51598c5da192a0c4cbf6fe4e3bebb
Étienne Beaulac <etienne.beaulac@outlook.com>
2023-02-11 14:16:14 -0500

    Removed unused import

--- gyro.py
+++ gyro.py
@@ -4,7 +4,6 @@ from abc import ABC, abstractmethod
 import hal
 import navx
 import wpilib
-import wpiutil
 from wpilib.simulation import SimDeviceSim
 from wpimath.geometry import Rotation2d
 from wpiutil import Sendable, SendableBuilder
