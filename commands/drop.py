76a69948c12d6f20981188df276c8bae4e8b154b
Thomas McDonald <4001137@keranna.qc.ca>
2023-02-11 11:29:50 -0500

    Added commands Drop and TakeObject

---
+++ commands/drop.py
@@ -0,0 +1,15 @@
+from subsystems.claw import Claw
+from subsystems.arm import Arm
+
+from commands.openclaw import OpenClaw
+from commands.movearm import MoveArm
+
+from commands2 import SequentialCommandGroup
+
+
+class Drop(SequentialCommandGroup):
+    def __init__(self, claw: Claw, arm: Arm):
+        super().__init__(
+            OpenClaw(claw),
+            MoveArm.toBase(arm)
+        )
