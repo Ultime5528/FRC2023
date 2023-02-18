11f08b57f573c3d6ea764143007edfaa3c8a5254
Étienne Beaulac <etiennebeaulac@users.noreply.github.com>
2023-02-18 11:53:39 -0500

    Added manual Arm commands (#90)

--- robot.py
+++ robot.py
@@ -6,6 +6,13 @@ from commands2.button import JoystickButton
 
 from commands.basicfollowtrajectory import BasicFollowTrajectory
 from commands.drive import Drive
+from commands.manualelevate import ManualElevate
+from commands.movearm import MoveArm
+from subsystems.arm import Arm
+from subsystems.drivetrain import Drivetrain
+from commands.followtrajectory import FollowTrajectory
+from subsystems.arm import Arm
+from commands.movearm import MoveArm
 from utils.property import clear_autoproperties
 
 
@@ -22,6 +29,8 @@ class Robot(commands2.TimedCommandRobot):
 
         self.drivetrain.setDefaultCommand(Drive(self.drivetrain, self.stick))
         JoystickButton(self.stick, 1).whenPressed(MoveArm(self.arm, 2, 2))
+        JoystickButton(self.stick, 2).whenPressed(ManualElevate.up(self.arm))
+        JoystickButton(self.stick, 3).whenPressed(ManualElevate.down(self.arm))
         # Doit être à la fin, après que tout ait été instancié
         clear_autoproperties()
 
