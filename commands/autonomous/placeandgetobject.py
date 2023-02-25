27b96a1bc1f74e1bd59ab3f94c9e594b6b563e83
LateAlways <javiparedes08@outlook.com>
2023-02-25 14:32:36 -0500

    Autonomous wokring!

--- commands/autonomous/placeandgetobject.py
+++ commands/autonomous/placeandgetobject.py
@@ -1,26 +1,15 @@
 import commands2
 import wpilib
 
-from commands.autonomous.redplaceandgetobject import RedPlaceAndGetObject
-from commands.autonomous.blueplaceandgetobject import BluePlaceAndGetObject
+from commands.autonomous import redplaceandgetobject, blueplaceandgetobject
 from subsystems.arm import Arm
 from subsystems.claw import Claw
 from subsystems.drivetrain import Drivetrain
 from utils.safecommand import SafeCommand
 
 
-class PlaceAndGetObject(SafeCommand):
+class PlaceAndGetObject(commands2.ConditionalCommand):
     def __init__(self, drivetrain: Drivetrain, claw: Claw, arm: Arm):
-        super().__init__()
-        self.drivetrain = drivetrain
-        self.claw = claw
-        self.arm = arm
-
-    def execute(self) -> None:
-        if wpilib.DriverStation.getAlliance() == wpilib.DriverStation.Alliance.kRed:
-            RedPlaceAndGetObject(self.drivetrain, self.claw, self.arm)
-        elif wpilib.DriverStation.getAlliance() == wpilib.DriverStation.Alliance.kBlue:
-            BluePlaceAndGetObject(self.drivetrain, self.claw, self.arm)
-
-    def isFinished(self) -> bool:
-        return True
+        super().__init__(blueplaceandgetobject.BluePlaceAndGetObject(drivetrain, claw, arm),
+                         redplaceandgetobject.RedPlaceAndGetObject(drivetrain, claw, arm),
+                         lambda: wpilib.DriverStation.getAlliance() == wpilib.DriverStation.Alliance.kBlue)
