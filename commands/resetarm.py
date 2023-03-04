bb821e0ff194c7f48896af032467e7ef70453ea6
Étienne Beaulac <etiennebeaulac@users.noreply.github.com>
2023-03-04 14:53:00 -0500

    111 revert utilisation des encodeur et des limitswitchs pour le bras (#114)
    * Revert
    
    * Fix ResetArm
    
    * Added movearm positions and hotfixes
    
    ---------
    
    Co-authored-by: Maxence <3800945@keranna.qc.ca>
    Co-authored-by: Felix Albert <filalbert16@outlook.com>

--- commands/resetarm.py
+++ commands/resetarm.py
@@ -11,40 +11,20 @@ class ResetArm(SafeCommand):
         super().__init__()
         self.arm = arm
         self.addRequirements(self.arm)
-        self._has_reset_extension = False
-        self._has_reset_elevator = False
-
-    def initialize(self) -> None:
-        self._has_reset_extension = False
-        self._has_reset_elevator = False
-        self.arm._elevator_offset = float("-inf")
-        self.arm._extension_offset = float("-inf")
 
     def execute(self) -> None:
-        if not self._has_reset_elevator:
-            if self.arm._elevator_offset > float("-inf"):
-                self._has_reset_elevator = True
-                self.arm.motor_elevator.set(0.0)
-            elif self.arm.isSwitchElevatorMinOn():
-                self.arm.motor_elevator.set(self.elevator_speed)
-            else:
-                self.arm.motor_elevator.set(-self.elevator_speed)
+        if not self.arm.isSwitchElevatorMinOn():
+            self.arm.setElevatorSpeed(-self.elevator_speed)
         else:
-            self.arm.motor_elevator.set(0.0)
+            self.arm.setElevatorSpeed(0.0)
 
-        if not self._has_reset_extension:
-            if self.arm._extension_offset > float("-inf"):
-                self._has_reset_extension = True
-                self.arm.motor_extension.set(0.0)
-            elif self.arm.isSwitchExtensionMinOn():
-                self.arm.motor_extension.set(self.extension_speed)
-            else:
-                self.arm.motor_extension.set(-self.extension_speed)
+        if not self.arm.isSwitchExtensionMinOn():
+            self.arm.setExtensionSpeed(-self.extension_speed)
         else:
-            self.arm.motor_extension.set(0.0)
+            self.arm.setExtensionSpeed(0.0)
 
     def isFinished(self) -> bool:
-        return self._has_reset_extension and self._has_reset_elevator
+        return self.arm.isSwitchElevatorMinOn() and self.arm.isSwitchExtensionMinOn()
 
     def end(self, interrupted: bool) -> None:
         self.arm.setElevatorSpeed(0.0)
