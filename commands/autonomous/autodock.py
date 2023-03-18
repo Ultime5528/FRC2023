70b83c628d66256fecc25c168e9f501a34d02b37
Thomas McDonald <4001137@keranna.qc.ca>
2023-03-18 10:30:50 -0400

    Drivetodock pusle

--- commands/autonomous/autodock.py
+++ commands/autonomous/autodock.py
@@ -2,6 +2,7 @@ import commands2
 
 from commands.autonomous.autodrop import AutoDrop
 from commands.drivetodock import DriveToDock
+from commands.movearm import MoveArm
 from subsystems.drivetrain import Drivetrain
 from subsystems.claw import Claw
 from subsystems.arm import Arm
@@ -12,7 +13,13 @@ class AutoDock(commands2.SequentialCommandGroup):
     backwards_distance = autoproperty(10.0)
 
     def __init__(self, drivetrain: Drivetrain, claw: Claw, arm: Arm, drop: bool):
-        commands = [DriveToDock(drivetrain, True)]
+        commands = [commands2.ParallelCommandGroup(
+            MoveArm.toBase(arm),
+            commands2.SequentialCommandGroup(
+                commands2.WaitCommand(1),
+                DriveToDock(drivetrain, True)
+            )
+        )]
         if drop:
             commands.insert(0, AutoDrop(claw, arm))
 
